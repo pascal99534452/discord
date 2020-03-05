a51b74d9d7a147d4b2f358dfe226bcf88241755b
pascal99534452 <farikored.info@gmail.com>
2020-03-05 21:09:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ bot.on("message", async message => {
 });
 bot.on("guildMemberAdd", member => {
 
-   const channel = member.guild.channels.find("name", "u");
+   const channel = member.guild.channels.find("name", "👋🏻・welcome");
    if (!channel) console.log("Dit kanaal bestaat niet");
 
    var joinMessage = new discord.RichEmbed()
