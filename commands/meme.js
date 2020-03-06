9f18b9afaf121fa15c7dc151b15fec665269dbc6
pascal99534452 <farikored.info@gmail.com>
2020-03-06 14:47:54 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,17 +1,15 @@
 const { RichEmbed } = require("discord.js")
-const { cyan } = require("../../colours.json");
 const fetch = require('node-fetch');
 
-module.exports = { 
-    run: async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Generating...")
 
     fetch("https://apis.duncte123.me/meme")
     .then(res => res.json()).then(body => {
-        if(!body) return message.reply("whoops! I've broke, try again!")
+        if(!body) return message.reply("Oeps! Het werkt even niet, probeer het opnieuw!")
 
         let mEmbed = new RichEmbed()
-        .setColor(cyan)
+        .setColor('#009999')
         .setTitle("Memes! :joy:")
         .setImage(body.data.image)
         .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp()
@@ -19,7 +17,6 @@ module.exports = {
             message.channel.send(mEmbed)
             msg.delete();
         })
-    }
 }
 
 module.exports.help = {
