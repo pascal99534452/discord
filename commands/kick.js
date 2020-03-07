70b725b82a91fafedd2bb34e7a0bbac625f5e779
pascal99534452 <farikored.info@gmail.com>
2020-03-07 21:29:47 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,34 +2,40 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    var KickUser = message.guild.member(message.mentions.users.first() || message.guild.member(arguments[0]));
-
-    if(!KickUser) return message.channel.send(":no_entry: | Voer alle argumenten in!")
-
-    var reason = args.join(" ").slice(22);
-    message.channel.send("Je hebt " + KickUser + " Gekicked.")
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij hebt geen toegang tot dit commando!");
-
-    if(KickUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij kan geen staffleden verwijderen!");
-    
-    var kick = new discord.RichEmbed()
-    .setTitle("Kick Systeem")
-    .setColor("#009999")
-    .addField("Speler:", KickUser)
-    .addField("Gekicked door:", message.author)
-    .addField("Reden:", reason)
-    .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
-
     var kickChannel = message.guild.channels.find(`name`, "⛔・logs");
-    if (!kickChannel) return message.guild.send("Kan het kanaal niet vinden! :frowning:")
+    let member = message.mentions.members.first();
 
-    message.guild.member(KickUser).kick(reason);
+    if (!message.member.roles.some(r => ["💼 ・Beheerder", "", "", ""].includes(r.name)))
+        return message.reply(":no_entry: | Jij hebt geen toegang tot dit commando!");
+    if (!member)
+        return message.reply(":no_entry: | Je moet wel een speler kiezen die je wilt kicken!");
+    if (!member.kickable)
+        return message.reply(":no_entry: | Jij kan geen staffleden kicken!");
 
-    kickChannel.send(kick)
+    let reason = args.slice(1).join(' ');
+    if (!reason) reason = "Geen reden gevonden";
 
-    return;
+    await member.kick(reason)
+        .catch(error => message.reply(`Sorry ${message.author} Ik kan deze persoon niet kicken: ${error}`));
+
+    var kickk = new discord.RichEmbed()
+        .setTitle("Kick Systeem")    
+        .setColor("#009999")
+        .addField("Speler:", member)
+        .addField("Gekicked door:", message.author)
+        .addField("Reden:", reason)
+        .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
+
+        var kicknn = new discord.RichEmbed()
+        .setTitle("Kick Systeem")    
+        .setColor("#009999")
+        .addField("Speler:", member)
+        .addField("Gekicked door:", message.author)
+        .addField("Reden:", reason)
+        .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
+    kickChannel.send(kickk)
+    message.channel.send(kicknn)
 }
-
 module.exports.help = {
-name: "kick"
+    name: "kick"
 }
