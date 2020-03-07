b9881770c535961d6eadd187034c207388c1bd94
pascal99534452 <farikored.info@gmail.com>
2020-03-07 20:34:42 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,58 +1,35 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
 
-    const user = message.mentions.users.first(); // returns the user object if an user mention exists
-    const banReason = args.slice(1).join(' '); // Reason of the ban (Everything behind the mention)
+    var banUser = message.guild.member(message.mentions.users.first() || message.guild.member(arguments[0]));
 
-    // Check if an user mention exists in this message
-    if (!user) {
-        try {
-            // Check if a valid userID has been entered instead of a Discord user mention
-            if (!message.guild.members.get(args.slice(0, 1).join(' '))) throw new Error('Couldn\' get a Discord user with this userID!');
-            // If the client (bot) can get a user with this userID, it overwrites the current user variable to the user object that the client fetched
-            user = message.guild.members.get(args.slice(0, 1).join(' '));
-            user = user.user;
-        } catch (error) {
-            return message.reply('Couldn\' get a Discord user with this userID!');
+    if(!banUser) return message.channel.send(":no_entry: | Voer alle argumenten in!")
 
-            if (user === message.author) return message.channel.send('You can\'t ban yourself'); // Check if the user mention or the entered userID is the message author himsmelf
-            if (!reason) return message.reply('You forgot to enter a reason for this ban!'); // Check if a reason has been given by the message author
-            if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user because you the bot has not sufficient permissions!'); // Check if the user is bannable with the bot's permissions
+    var reason = args.join(" ").slice(22);
+    message.channel.send("Je hebt " + banUser + " Gebanned.")
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij hebt geen toegang tot dit commando!");
 
-            await message.guild.ban(user) // Bans the user
+    if(banUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij kan geen staffleden verbannen!");
+    
+    var ban = new discord.RichEmbed()
+    .setTitle("Ban Systeem")
+    .setColor("#009999")
+    .addField("Speler:", banUser)
+    .addField("Gebanned door:", message.author)
+    .addField("Reden:", reason)
+    .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
 
-            const Discord = require('discord.js'); // We need Discord for our next RichEmbeds
-            const banConfirmationEmbed = new Discord.RichEmbed()
-                .setColor('RED')
-                .setDescription(`✅ ${user.tag} has been successfully banned!`);
-            message.channel.send({
-                embed: banConfirmationEmbed
-            }); // Sends a confirmation embed that the user has been successfully banned
+    var banChannel = message.guild.channels.find(`name`, "⛔・logs");
+    if (!banChannel) return message.guild.send("Kan het kanaal niet vinden! :frowning:")
 
+    message.guild.member(banUser).ban(reason);
 
-            const modlogChannelID = ''; // Discord channel ID where you want to have logged the details about the ban
-            if (modlogChannelID.length !== 0) {
-                if (!client.channels.get(modlogChannelID)) return undefined; // Check if the modlogChannelID is a real Discord server channel that really exists
+    banChannel.send(ban)
 
-                const banConfirmationEmbedModlog = new Discord.RichEmbed()
-                    .setAuthor(`Banned by **${msg.author.username}#${msg.author.discriminator}**`, msg.author.displayAvatarURL)
-                    .setThumbnail(user.displayAvatarURL)
-                    .setColor('RED')
-                    .setTimestamp()
-                    .setDescription(`**Action**: Ban
-**User**: ${user.username}#${user.discriminator} (${user.id})
-**Reason**: ${reason}`);
-                client.channels.get(modlogChannelID).send({
-                    embed: banConfirmationEmbedModlog
-
-                })
-            }
-        }
-    }
+    return;
 }
 
 module.exports.help = {
-                        name: "ban"
+name: "ban"
 }
