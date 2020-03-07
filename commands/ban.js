289d54bcde03b9e12cb3256c8009746ad8839cd1
pascal99534452 <farikored.info@gmail.com>
2020-03-07 20:13:34 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,20 +4,23 @@ module.exports.run = async (bot, message, args) => {
 
     var banUser = message.guild.member(message.mentions.users.first() || message.guild.member(arguments[0]));
 
-    if(!banUser) return message.channel.send(":no_entry: | Voer alle argumenten in!")
+    if (!banUser) return message.channel.send(":no_entry: | Voer alle argumenten in!")
 
     var reason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij hebt geen toegang tot dit commando!");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij hebt geen toegang tot dit commando!");
 
-    if(banUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij kan geen staffleden verbannen!");
+
+    if (banUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":no_entry: | Jij kan geen staffleden verbannen!");
+
     var bann = message.channel.send("Je hebt " + banUser + " Gebanned.")
+
     var ban = new discord.RichEmbed()
-    .setTitle("Ban Systeem")
-    .setColor("#009999")
-    .addField("Speler:", banUser)
-    .addField("Gebanned door:", message.author)
-    .addField("Reden:", reason)
-    .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
+        .setTitle("Ban Systeem")
+        .setColor("#009999")
+        .addField("Speler:", banUser)
+        .addField("Gebanned door:", message.author)
+        .addField("Reden:", reason)
+        .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp();
 
     var banChannel = message.guild.channels.find(`name`, "⛔・logs");
     if (!banChannel) return message.guild.send("Kan het kanaal niet vinden! :frowning:")
@@ -25,10 +28,10 @@ module.exports.run = async (bot, message, args) => {
     message.guild.member(banUser).ban(reason);
 
     banChannel.send(ban, bann)
- 
+
     return;
 }
 
 module.exports.help = {
-name: "ban"
+    name: "ban"
 }
