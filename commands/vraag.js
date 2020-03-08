801830473795db04e46d435c05c9f1a725360527
pascal99534452 <farikored.info@gmail.com>
2020-03-08 21:06:16 +0100

    Update vraag.js

--- commands/vraag.js
+++ commands/vraag.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     function doMagic8BallVoodoo() {
-    var rand = ['Ik weet het niet zeker', 'Misschien wel.', 'Mijn hond heeft dorst.', 'Zonder twijfel.', 'Vraag het aan pieter.', 'Misschien niet.', 'Ik denk het.', 'Geen idee.', 'Sinterklaas weet het wel beter.', 'NOOIT!', 'Auke stinkt echt.', 'Ajax is gay.', 'Mijn muziek staat te hard.', 'Het gaat goed.', 'Ja.', 'Nee.', 'Ik weet het niet.', 'Jou vraag boeit niemand.', 'Precies'];
-
-    return rand[Math.floor(Math.random()*rand.length)];
-    let question = args.slice(0).join(" ");
+        var rand = ['Yes', 'No', 'Why are you even trying?', 'What do you think? NO', 'Maybe', 'Never', 'Yep'];
+    
+        return rand[Math.floor(Math.random()*rand.length)];
+    }
+    let question = arguments.slice(0).join(" ");
 
     let embed = new Discord.RichEmbed()
     .setTitle("8Ball")
@@ -14,9 +15,8 @@ module.exports.run = async (bot, message, args) => {
     .addField("Antwoord:", doMagic8BallVoodoo())
     .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
 
-    message.channel.send({embed});
+    message.channel.send(embed);
 
-    }
 }
 
 module.exports.help = {
