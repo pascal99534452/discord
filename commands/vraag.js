6fef48a63d874501230184b40f8dc66e7f51860c
pascal99534452 <farikored.info@gmail.com>
2020-03-08 19:50:23 +0100

    Update vraag.js

--- commands/vraag.js
+++ commands/vraag.js
@@ -1,27 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let replies = [
-        'Ik weet het niet zeker',
-	    'Misschien wel.',
-	    'Mijn hond heeft dorst.',
-	    'Zonder twijfel.',
-    	'Vraag het aan pieter.',
-	    'Misschien niet.',
-    	'Ik denk het.',
-    	'Geen idee.',
-    	'Sinterklaas weet het wel beter.',
-    	'NOOIT!',
-    	'Auke stinkt echt.',
-	    'Ajax is gay.',
-    	'Mijn muziek staat te hard.',
-    	'Het gaat goed.',
-    	'Ja.',
-	    'Nee.',
-	    'Ik weet het niet.',
-    	'Jou vraag boeit niemand.',
-    	'Precies',
-    ];
+    let replies = ['Ik weet het niet zeker', 'Misschien wel.', 'Mijn hond heeft dorst.', 'Zonder twijfel.', 'Vraag het aan pieter.', 'Misschien niet.', 'Ik denk het.', 'Geen idee.', 'Sinterklaas weet het wel beter.', 'NOOIT!', 'Auke stinkt echt.', 'Ajax is gay.', 'Mijn muziek staat te hard.', 'Het gaat goed.', 'Ja.', 'Nee.', 'Ik weet het niet.', 'Jou vraag boeit niemand.', 'Precies'];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(0).join(" ");
@@ -30,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("8Ball")
     .setColor('#009999')
     .addField("Vraag:", question)
-    .addField("Antwoord:", onderhoud)
+    .addField("Antwoord:", result)
     .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
 
     message.channel.send({embed});
