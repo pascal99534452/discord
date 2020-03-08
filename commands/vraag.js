f1f1ab6cd3428e2489a691fef5d5cb556bc17e25
pascal99534452 <farikored.info@gmail.com>
2020-03-08 19:59:09 +0100

    Update vraag.js
    g

--- commands/vraag.js
+++ commands/vraag.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let replies = ['Ik weet het niet zeker', 'Misschien wel.', 'Mijn hond heeft dorst.', 'Zonder twijfel.', 'Vraag het aan pieter.', 'Misschien niet.', 'Ik denk het.', 'Geen idee.', 'Sinterklaas weet het wel beter.', 'NOOIT!', 'Auke stinkt echt.', 'Ajax is gay.', 'Mijn muziek staat te hard.', 'Het gaat goed.', 'Ja.', 'Nee.', 'Ik weet het niet.', 'Jou vraag boeit niemand.', 'Precies'];
 
-    let result = Math.floor((Math.random() * replies.length));
+    let rand = [Math.floor(Math.random()*rand.length)];
     let question = args.slice(0).join(" ");
 
     let embed = new Discord.RichEmbed()
