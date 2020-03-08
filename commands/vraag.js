f7feea5cbc988a00f8621b48874d32419712e159
pascal99534452 <farikored.info@gmail.com>
2020-03-08 20:22:00 +0100

    Update vraag.js

--- commands/vraag.js
+++ commands/vraag.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let replies = ['Ik weet het niet zeker', 'Misschien wel.', 'Mijn hond heeft dorst.', 'Zonder twijfel.', 'Vraag het aan pieter.', 'Misschien niet.', 'Ik denk het.', 'Geen idee.', 'Sinterklaas weet het wel beter.', 'NOOIT!', 'Auke stinkt echt.', 'Ajax is gay.', 'Mijn muziek staat te hard.', 'Het gaat goed.', 'Ja.', 'Nee.', 'Ik weet het niet.', 'Jou vraag boeit niemand.', 'Precies'];
+    function doMagic8BallVoodoo() {
+    var rand = ['Ik weet het niet zeker', 'Misschien wel.', 'Mijn hond heeft dorst.', 'Zonder twijfel.', 'Vraag het aan pieter.', 'Misschien niet.', 'Ik denk het.', 'Geen idee.', 'Sinterklaas weet het wel beter.', 'NOOIT!', 'Auke stinkt echt.', 'Ajax is gay.', 'Mijn muziek staat te hard.', 'Het gaat goed.', 'Ja.', 'Nee.', 'Ik weet het niet.', 'Jou vraag boeit niemand.', 'Precies'];
 
-    let rand = [Math.floor(Math.random()*rand.length)];
+    return rand[Math.floor(Math.random()*rand.length)];
     let question = args.slice(0).join(" ");
 
     let embed = new Discord.RichEmbed()
     .setTitle("8Ball")
     .setColor('#009999')
     .addField("Vraag:", question)
-    .addField("Antwoord:", rand)
+    .addField("Antwoord:", doMagic8BallVoodoo());
     .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
 
     message.channel.send({embed});
 
+    }
 }
 
 module.exports.help = {
