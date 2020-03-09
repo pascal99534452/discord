04e180b9a23193a444313d6d40e24374e16a309d
pascal99534452 <farikored.info@gmail.com>
2020-03-09 20:42:06 +0100

    g
    g

---
+++ commands/auke.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    var member = message.member.user.tag;
+    
+    var botEmbed = new discord.RichEmbed()
+        .setTitle("**AUKUHH**")
+        .setDescription("Auke stinkt")
+        .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
+        .setColor('#009999');
+
+    return message.channel.send(botEmbed);
+
+}
+
+module.exports.help = {
+name: "auke"
+}
