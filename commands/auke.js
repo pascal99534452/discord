bf276aac903ef9bc68761abd8f680015e98efd0a
pascal99534452 <farikored.info@gmail.com>
2020-03-09 20:49:35 +0100

    Update auke.js

--- commands/auke.js
+++ commands/auke.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
     var member = message.member.user.tag;
     
     var botEmbed = new discord.RichEmbed()
-        .setTitle("**AUKUHH**")
-        .setDescription("Auke stinkt")
+        .setTitle('**AUKUHH**')
+        .setDescription('Auke stinkt')
         .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
         .setColor('#009999');
 
