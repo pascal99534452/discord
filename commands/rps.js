0a7b7987d26ee1904eff3a1c788f5a96057ad2f3
pascal99534452 <farikored.info@gmail.com>
2020-03-09 21:04:22 +0100

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,30 +1,53 @@
 const Discord = module.require("discord.js");
 
 function rand(low, high) {
-    return Math.random() * (high + 1 - low) + low | 0;
+  return Math.random() * (high + 1 - low) + low | 0;
 }
 
-module.exports.run = async (bot, message, args) =>{
+module.exports.run = async (bot, message, args) => {
 
   if (args[0]) {
-        // get user choice && user choice
-        let computer_choice = rand(0,2);
-        let user_choice = args[0] == "rock" ? 1 : args[0] == "paper" ? 2 : 0;
+    var gelijkspell = new discord.RichEmbed()
+      .setTitle('**Steen, papier, schaar!**')
+      .setDescription('Het is gelijkspel! :frowning: ')
+      .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp()
+      .setColor('#009999');
 
-        // if their choices are same its a draw :D
-        if (computer_choice == user_choice) {
-          message.reply("Het is gelijkspel! :frowning:");
-        }
-        else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
-          message.reply("Bot heeft gewonnen! :tada:");
-        } else {
-          message.reply("Jij hebt gewonnen! :tada:");
-        }
-      }
+    var gewonnenn = new discord.RichEmbed()
+      .setTitle('**Steen, papier, schaar!**')
+      .setDescription('Je hebt gewonnen! :tada: ')
+      .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp()
+      .setColor('#009999');
 
-      if (!args[0]) {
-        message.reply("!rps rock/paper/scissors");
-      }
+    var verlorenn = new discord.RichEmbed()
+      .setTitle('**Steen, papier, schaar!**')
+      .setDescription('Helaas je hebt verloren! :frowning: ')
+      .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp()
+      .setColor('#009999');
+
+    var argss = new discord.RichEmbed()
+      .setTitle('**Steen, papier, schaar!**')
+      .setDescription('Gebruik: !rps steen , papier , schaar! :slight_smile: ')
+      .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1').setTimestamp()
+      .setColor('#009999');
+    // get user choice && user choice
+    let computer_choice = rand(0, 2);
+    let user_choice = args[0] == "rock" ? 1 : args[0] == "paper" ? 2 : 0;
+
+    // if their choices are same its a draw :D
+    if (computer_choice == user_choice) {
+      message.reply(gelijkspell);
+    }
+    else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
+      message.reply(verlorenn);
+    } else {
+      message.reply(gewonnenn);
+    }
+  }
+
+  if (!args[0]) {
+    message.reply(argss);
+  }
 }
 
 module.exports.help = {
