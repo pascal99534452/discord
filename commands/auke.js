fa92a5144591d70a2060d7dc526c9636e7505bdd
pascal99534452 <farikored.info@gmail.com>
2020-03-09 20:22:13 +0100

    g
    g

---
+++ commands/auke.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    var botIcon = bot.user.displayAvatarURL
+
+    var botEmbed = new discord.RichEmbed()
+        .setTitle("AUKUHH")
+        .setDescription("Auke stinkt naar kattenvoer :(")
+        .setFooter('UnitedMC', 'https://i.imgur.com/7A0DkcB.png?1') .setTimestamp() 
+        .setColor('#009999');
+
+    return message.channel.send(botEmbed);
+
+}
+
+module.exports.help = {
+name: "auke"
+}
